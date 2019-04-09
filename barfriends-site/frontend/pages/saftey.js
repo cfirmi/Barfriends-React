69c1d18d060ae344942ad371053399d08c0e0cb1
christian <cpalvetzian@gmail.com>
2019-04-09 00:34:19 -0400

    QRCode

---
+++ barfriends-site/frontend/pages/saftey.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const BFBusiness = () => (
+  <div style={{marginTop: '100px', color: 'white'}}>
+    <h1>
+      This is the safe page
+    </h1>
+  </div>
+);
+
+export default BFBusiness;
